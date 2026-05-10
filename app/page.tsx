dfd59306e97e5998bd6fb11e8498d9667c364a78
Miroslav Kucera <mkdigitalproduct@gmail.com>
2026-05-10 15:27:39 +0200

    redirect homepage to shop

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>MK Digital Studio 🚀</h1>
-      <p>Portfolio landing běží správně.</p>
+import { redirect } from "next/navigation";
 
-      <div style={{ marginTop: 20, display: "flex", gap: 16 }}>
-        <a href="/shop">Otevřít e-shop demo</a>
-      </div>
-    </main>
-  );
+export default function Home() {
+  redirect("/shop");
 }
